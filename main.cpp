dd64b9d4b711521506aefd6daf55232853f9d69c
Santiago Ducos <santidu200@gmail.com>
2022-02-10 20:03:15 -0300

    agrego algunas funciones al modulo lista_strings

--- main.cpp
+++ main.cpp
@@ -3,68 +3,49 @@
 
 int main() {
 
+    ABBSecuencias abb = crearArbolDeSecuencias();
 
-    string test = string(" a");
+    // create pepe
+    create(abb, string("pepe"));
 
-    mostrarListaDeStrings(convertirStringEnUnaListaDeStrings(test));
+    // insback pepe 4
+    insback(abb, string("pepe"), 4);
 
-//    ListaStrings lista = crearListaDeStrings();
-//
-//    agregarStringAListaDeStrings(lista, string("ola"));
-//
-//    agregarStringAListaDeStrings(lista, string("mamuu"));
-//
-//    agregarStringAListaDeStrings(lista, string("como estas"));
-//
-//    printf("\n%i", tamanioDeListaDeStrings(lista));
-//
-//    mostrarListaDeStrings(lista);
+    // insback pepe 5
+    insback(abb, string("pepe"), 5);
 
+    // insback pepe 2
+    insback(abb, string("pepe"), 2);
 
+    // create lolo
+    create(abb, string("lolo"));
 
-//    ABBSecuencias abb = crearArbolDeSecuencias();
-//
-//    // create pepe
-//    create(abb, string("pepe"));
-//
-//    // insback pepe 4
-//    insback(abb, string("pepe"), 4);
-//
-//    // insback pepe 5
-//    insback(abb, string("pepe"), 5);
-//
-//    // insback pepe 2
-//    insback(abb, string("pepe"), 2);
-//
-//    // create lolo
-//    create(abb, string("lolo"));
-//
-//    // insback lolo 3
-//    insback(abb, string("lolo"), 3);
-//
-//    // insback lolo 5
-//    insback(abb, string("lolo"), 5);
-//
-//    // concat pepe lolo momo
-//    concat(abb, string("pepe"), string("lolo"), string("concatenacion"));
-//
-//    // reverse pepe invpepe
-//    reverse(abb, string("pepe"), string("invpepe"));
-//
-//    // suma
-//    suma(abb, string("pepe"));
-//
-//    // save pepe secuencias.txt
-//    save(abb, string("pepe"), string("secuencias.txt"));
-//
-//    // load oli secuencias.txt
-//    load(abb, string("secuencias.txt"), string("oli"));
-//
-//    // show
-//    show(abb);
-//
-//    // exit
-//    exit(abb);
-//
-//    return 0;
+    // insback lolo 3
+    insback(abb, string("lolo"), 3);
+
+    // insback lolo 5
+    insback(abb, string("lolo"), 5);
+
+    // concat pepe lolo momo
+    concat(abb, string("pepe"), string("lolo"), string("concatenacion"));
+
+    // reverse pepe invpepe
+    reverse(abb, string("pepe"), string("invpepe"));
+
+    // suma
+    suma(abb, string("pepe"));
+
+    // save pepe secuencias.txt
+    save(abb, string("pepe"), string("secuencias.txt"));
+
+    // load oli secuencias.txt
+    load(abb, string("secuencias.txt"), string("oli"));
+
+    // show
+    show(abb);
+
+    // exit
+    exit(abb);
+
+    return 0;
 }
